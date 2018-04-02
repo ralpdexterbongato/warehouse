e58c7ffef35349eb8a4a300bc54c2eb071d3f229
ralpdexterbongato <rdc2898@gmail.com>
2018-04-02 16:17:51 +0800

    deployment online config

--- config/database.php
+++ config/database.php
@@ -1,5 +1,9 @@
 <?php
-
+$url =  parse_url(getenv("DATABASE_URL"));
+$host = $url["host"]??null;
+$username = $url["user"]??null;
+$password = $url["pass"]??null;
+$database = substr($url["path"], 1)??null;
 return [
 
     /*
@@ -13,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql_production'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,6 +69,17 @@ return [
             'prefix' => '',
             'schema' => 'public',
             'sslmode' => 'prefer',
+        ],
+        
+        'pgsql_production' => [
+            'driver' => 'pgsql',
+            'host' => $host,
+            'database' => $database,
+            'username' => $username, //production to postgre heroku
+            'password' => $password,
+            'charset' => 'utf8',
+            'prefix' => '',
+            'schema' => 'public',
         ],
         'sqlsrv' => [
           'driver' => 'sqlsrv',
