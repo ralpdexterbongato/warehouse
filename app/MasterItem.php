1b1d84caefc1d8353000e74fa17518b167345ee1
ralpdexterbongato <rdc2898@gmail.com>
2018-04-03 10:36:39 +0800

    update model

--- app/MasterItem.php
+++ app/MasterItem.php
@@ -9,6 +9,7 @@ class MasterItem extends Model
     protected $table = 'MasterItems';
     public $timestamps = false;
     public $increments=false;
+    public $primaryKey='ItemCode';
     public function MaterialsTicketDetails()
     {
       return $this->hasMany('App\MaterialsTicketDetail','ItemCode','ItemCode');
