84ad1cc2a7ae56ef6829c8d8e171d5d2912a6036
ralpdexterbongato <rdc2898@gmail.com>
2018-04-09 14:37:20 +0800

    added db

--- app/User.php
+++ app/User.php
@@ -6,6 +6,7 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Auth;
 use Carbon\Carbon;
+use DB;
 class User extends Authenticatable
 {
     use Notifiable;
